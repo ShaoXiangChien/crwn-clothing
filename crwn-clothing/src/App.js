995bd881544cf938861381bbd6037cd5214b8e28
簡紹翔 <jianshaoxiang@jianshaoxiangdeMacBook-Air.local>
2021-02-20 09:06:41 +0800

    commit on 'master'

---
+++ crwn-clothing/src/App.js
@@ -0,0 +1,12 @@
+import logo from "./logo.svg";
+import "./App.css";
+import HomePage from "./homepage.component.jsx";
+function App() {
+  return (
+    <div>
+      <HomePage />
+    </div>
+  );
+}
+
+export default App;
